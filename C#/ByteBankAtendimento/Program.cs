9f48d7b7b9c036ad6855d55b3baeda0caa2ce20d
Helena <helena.slva19@gmail.com>
2023-10-14 13:51:18 -0300

    add exercicios

--- C#/ByteBankAtendimento/Program.cs
+++ C#/ByteBankAtendimento/Program.cs
@@ -4,7 +4,72 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] idades = new int[5];
+            idades[0] = 30;
+            idades[1] = 40;
+            idades[2] = 20;
+            idades[3] = 10;
+            idades[4] = 40;
+
+
+            int RetornaMediaIdades(int[] idades)
+            {
+                int somaIdades = 0;
+                int media = 0;
+
+                for (int i = 0; i<idades.Length; i++)
+                {
+                   somaIdades += idades[i];
+                }
+
+                media = somaIdades / idades.Length;
+
+                return media;
+            }
+
+            void BuscarPalavras()
+            {
+                string[] palavras = new string[5];
+                Boolean encontrou = false;
+
+                for (int i = 0;i<palavras.Length;i++)
+                {
+                    Console.Write($"Digite a {i+1}ª palavra: ");
+                    palavras[i] = Console.ReadLine();
+                }
+
+                Console.WriteLine("Digite palavra a ser encontrada: ");
+                var busca = Console.ReadLine(); 
+
+                foreach (string s in palavras)
+                {
+                    if (s.Equals(busca))
+                    {
+                        encontrou = true;
+                    }
+                    
+                }
+                if (encontrou)
+                {
+                    Console.WriteLine($"Palavra encontrada = {busca}");
+                }
+                else
+                {
+                    Console.WriteLine("Palavra não encontrada");
+                }
+                
+            }
+
+            Array amostra = Array.CreateInstance(typeof(double), 5);
+            amostra.SetValue(5.9, 0);
+            amostra.SetValue(6.5, 0);
+            amostra.SetValue(3.2, 0);
+            amostra.SetValue(8.1, 0);
+            amostra.SetValue(10, 0);
+
+
+            Console.WriteLine(RetornaMediaIdades(idades));
+            BuscarPalavras();
         }
     }
 }
---
+++ C#/ProjetoDoisUdemy/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace ProjetoDoisUdemy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario();
+            Funcionario f2 = new Funcionario();
+
+            Console.WriteLine("Escreva o nome do primeiro funcionário: ");
+            f1.Nome = Console.ReadLine();
+            Console.WriteLine("Escreva o salario do primeiro funcionário: ");
+            f1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Escreva o nome do Segundo funcionário: ");
+            f2.Nome = Console.ReadLine();
+            Console.WriteLine("Escreva o salario do segundo funcionário: ");
+            f1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = (f1.Salario + f2.Salario) / 2.0;
+            Console.WriteLine("A média dos salários é: " + media.ToString("F2"), CultureInfo.InvariantCulture);
+
+        }
+    }
+}
