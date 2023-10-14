05448f91ce3599237e9656211d1ec45acfc28796
Helena <helena.slva19@gmail.com>
2023-10-14 18:21:44 -0300

    Add  exercicios

---
+++ C#/ProjetoCincoUdemy/ContaBancaria.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCincoUdemy
+{
+    internal class ContaBancaria
+    {
+        public ContaBancaria(long numero, string nome)
+        {
+            Numero = numero;
+            Nome = nome;
+            Saldo = 0.0;
+        }
+        public ContaBancaria(long numero, string nome, double saldo) : this(numero, nome)
+        {
+
+            Saldo = saldo;
+        }
+
+        public long Numero { get; private set; }
+        public string Nome { get; set;}
+        public double Saldo { get; private set; }
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+        public void Sacar(double valor)
+        {
+            Saldo -= (valor + 5.0);
+        }
+        public override string? ToString()
+        {
+            return "Conta: " + Numero + "\r\n" +
+                "Titular: " + Nome + "\r\n" +
+                "Saldo: R$" + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ C#/ProjetoCincoUdemy/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ProjetoCincoUdemy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+            Console.WriteLine("Entre com o número da conta: ");
+            long numero = long.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.WriteLine("Haverá depósito inicial? (s/n): ");
+            char resposta = char.Parse(Console.ReadLine());
+
+            if(resposta == 's')
+            {
+                Console.WriteLine("Digite o saldo inicial: ");
+                double saldoInicial = double.Parse(Console.ReadLine()); 
+                conta = new ContaBancaria(numero, titular, saldoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+               
+
+            Console.WriteLine(conta.ToString());
+        }
+    }
+}
